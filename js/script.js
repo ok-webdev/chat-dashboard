bd9d231e0825bd40ed199c6d659d04330633c0fb
ok-webdev <oleg.kokoshinskii@gmail.com>
2021-02-11 23:20:23 +1000

    result

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const chatFilter = document.querySelector('.chats__filter'),
+    chatFilterMenu = document.querySelector('.chats__filter-submenu'),
+    chatSearchFilter = document.querySelector('.chats__search-filter'),
+    chatSearchFilterMenu = document.querySelector('.chats__search-filter-submenu'),
+    panelMenu = document.querySelector('.chat__panel-menu'),
+    panelSubmenu = document.querySelector('.chat__panel-submenu');
+
+chatFilter.addEventListener('click', () => {
+    chatFilterMenu.classList.toggle('chats__filter-submenu_active');
+});
+
+chatSearchFilter.addEventListener('click', () => {
+    chatSearchFilterMenu.classList.toggle('chats__search-filter-submenu_active');
+});
+
+panelMenu.addEventListener('click', () => {
+  panelSubmenu.classList.toggle('chat__panel-submenu_active');
+}); 
